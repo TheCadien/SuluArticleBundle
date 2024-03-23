a93e442cd0d830ef584d629384b094d32b36cbe9
Oliver Kossin <oliver.kossin@qossmic.com>
2024-03-23 14:19:43 +0100

    set dimension attributes

--- src/Infrastructure/Sulu/Content/SingleArticleSelectionContentType.php
+++ src/Infrastructure/Sulu/Content/SingleArticleSelectionContentType.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Sulu\Article\Infrastructure\Sulu\Content;
 
 use Sulu\Article\Domain\Repository\ArticleRepositoryInterface;
+use Sulu\Article\Infrastructure\Doctrine\Repository\ArticleRepository;
+use Sulu\Bundle\ContentBundle\Content\Domain\Model\DimensionContentInterface;
 use Sulu\Bundle\WebsiteBundle\ReferenceStore\ReferenceStoreInterface;
 use Sulu\Component\Content\Compat\PropertyInterface;
 use Sulu\Component\Content\PreResolvableContentTypeInterface;
@@ -16,7 +18,7 @@ class SingleArticleSelectionContentType extends SimpleContentType implements Pre
 
     public function __construct(
         ArticleRepositoryInterface $articleRepository,
-        ReferenceStoreInterface $referenceStore,
+        ReferenceStoreInterface    $referenceStore,
     )
     {
         parent::__construct('Article');
@@ -32,7 +34,22 @@ class SingleArticleSelectionContentType extends SimpleContentType implements Pre
             return null;
         }
 
-        return $this->articleRepository->getOneBy(['uuid' => $uuid]);
+        $dimensionAttributes = [
+            'locale' => $property->getStructure()->getLanguageCode(),
+            'stage' => DimensionContentInterface::STAGE_DRAFT,
+        ];
+
+        return $this->articleRepository->getOneBy(
+            filters: \array_merge(
+                [
+                    'uuid' => $uuid,
+                    'load_ghost_content' => true,
+                ],
+                $dimensionAttributes,
+            ),
+            selects: [
+                ArticleRepositoryInterface::GROUP_SELECT_ARTICLE_ADMIN => true,
+            ]);
     }
 
     public function preResolve(PropertyInterface $property)
